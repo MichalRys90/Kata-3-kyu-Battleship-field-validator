c6c4a40e0d5d99850ef5b52582ce02dd2210de0f
Michal <mrys@tgo.pl>
2022-12-17 02:16:22 +0100

    Kata 3 kyu

--- src/Main.java
+++ src/Main.java
@@ -1,103 +1,181 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Mixing.mix(" In many languages", " there's a pair of functions"));
+        System.out.println(BattleField.fieldValidator(new int[][]{
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+                {0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
+                {1, 1, 1, 0, 1, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 1, 0, 1},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0, 1, 1},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0}}));
 
     }
 
-    public class Mixing {
+    public class BattleField {
 
-        public static String mix(String s1, String s2) {
-            String[] one = s1.split("");
-            String[] two = s2.split("");
+        public static boolean fieldValidator(int[][] field) {
+            int battleship = 0;
+            int cruisers = 0;
+            int destroyers = 0;
+            int submarines = 0;
+            int sum = 0;
+            for (int[] ints : field) {
+                System.out.println(Arrays.toString(ints));
+            }
 
-            Map<String, Integer> mapOne = new HashMap<>();
-            Map<String, Integer> mapTwo = new HashMap<>();
+            System.out.println();
 
-
-                for (String s : one) {
-                    if (mapOne.containsKey(s)) {
-                        mapOne.put(s, mapOne.get(s) + 1);
-                    } else {
-                        mapOne.put(s, 1);
+            for (int i = 1; i < field.length - 1; i++) {
+                for (int j = 1; j < 9; j++) {
+                    if (((field[i][j] == 1) && (field[i + 1][j]) == 1 && (field[i][j + 1] == 1) && (field[i + 1][j + 1] == 1))
+                            || ((field[i][j] == 1) && (field[i + 1][j]) == 1 && (field[i + 1][j + 1] == 1))
+                            || ((field[i][j] == 1) && (field[i + 1][j]) == 1 && (field[i + 1][j - 1] == 1))
+                            || ((field[i][j] == 1) && (field[i][j + 1]) == 1 && (field[i + 1][j + 1] == 1))
+                            || ((field[i][j] == 1) && (field[i][j + 1]) == 1 && (field[i + 1][j] == 1))
+                            || ((field[i][j] == 1) && (field[i + 1][j + 1] == 1))
+                            || ((field[i][j] == 1) && (field[i + 1][j - 1] == 1))) {
+                        return false;
                     }
-                }
-
-            Map<String, Integer> collect = mapOne.entrySet().stream()
-                    .filter(a -> a.getValue() > 1)
-                    .filter(a -> a.getKey().matches("[a-z]"))
-                    .collect(Collectors.toMap(
-                            Map.Entry::getKey,
-                            Map.Entry::getValue));
-
-            for (String s : two) {
-                if (mapTwo.containsKey(s)) {
-                    mapTwo.put(s, mapTwo.get(s) + 1);
-                } else {
-                    mapTwo.put(s, 1);
                 }
             }
 
-            Map<String, Integer> collect1 = mapTwo.entrySet().stream()
-                    .filter(a -> a.getValue() > 1)
-                    .filter(a -> a.getKey().matches("[a-z]"))
-                    .collect(Collectors.toMap(
-                            Map.Entry::getKey,
-                            Map.Entry::getValue));
+            for (int[] ints : field) {
+                for (int anInt : ints) {
+                    if (anInt == 1) {
+                        sum++;
+                        if (sum > 4) {
+                            return false;
+                        }
+                    } else {
+                        if (sum == 2) {
+                            destroyers++;
+                        } else if (sum == 3) {
+                            cruisers++;
+                        } else if (sum == 4) {
+                            battleship++;
+                        }
+                        sum = 0;
+                    }
+                }
+                if (sum == 2) {
+                    destroyers++;
+                } else if (sum == 3) {
+                    cruisers++;
+                } else if (sum == 4) {
+                    battleship++;
+                }
+                sum = 0;
+            }
 
-            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < field.length - 4; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if ((field[i][j] == 1) && (field[i + 1][j] == 1) && (field[i + 2][j] == 1)
+                            && (field[i + 3][j] == 1) && (field[i + 4][j] == 1)) {
+                        return false;
+                    }
+                }
+            }
 
-            for(Map.Entry<String, Integer> co : collect.entrySet()) {
-                for (Map.Entry<String, Integer> co1 : collect1.entrySet()) {
-                    if (co.getKey().equals(co1.getKey())) {
-                        if (co.getValue()>co1.getValue()) {
-                            str.append("1" + ":").append(String.join("", Collections.nCopies(co.getValue(), co.getKey()))).append("/");
-                        } else if (co1.getValue()>co.getValue()) {
-                            str.append("2" + ":").append(String.join("", Collections.nCopies(co1.getValue(), co1.getKey()))).append("/");
-                        } else {
-                            str.append("=" + ":").append(String.join("", Collections.nCopies(co1.getValue(), co1.getKey()))).append("/");
+            for (int i = 0; i < field.length - 3; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if ((field[i][j] == 1) && (field[i + 1][j] == 1) && (field[i + 2][j] == 1)
+                            && (field[i + 3][j] == 1)) {
+                        battleship++;
+                    }
+                }
+            }
 
+            for (int i = 0; i < field.length - 2; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (i > 0 && i < field.length - 3) {
+                        if (((field[i][j] == 1) && (field[i + 1][j] == 1) && (field[i + 2][j] == 1)
+                                && (field[i + 3][j] == 0)) && (field[i - 1][j] == 0)) {
+                            cruisers++;
+                        }
+                    } else if (i == 0) {
+                        if ((field[i][j] == 1) && (field[i + 1][j] == 1) && (field[i + 2][j] == 1)
+                                && (field[i + 3][j] == 0)) {
+                            cruisers++;
+                        }
+                    } else {
+                        if ((field[i][j] == 1) && (field[i + 1][j] == 1) && (field[i + 2][j] == 1)
+                                && (field[i - 1][j] == 0)) {
+                            cruisers++;
                         }
                     }
                 }
             }
 
-            for(Map.Entry<String, Integer> co : collect.entrySet()) {
-                if (!collect1.containsKey(co.getKey())) {
-                    str.append("1" + ":").append(String.join("", Collections.nCopies(co.getValue(), co.getKey()))).append("/");
-                }
-            }
-
-            for (Map.Entry<String, Integer> co1 : collect1.entrySet()) {
-                if (!collect.containsKey(co1.getKey())) {
-                    str.append("2" + ":").append(String.join("", Collections.nCopies(co1.getValue(), co1.getKey()))).append("/");
-                }
-            }
-
-            String[] a = str.toString().split("/");
-            for(int i = 0; i < a.length - 1; ++i) {
-                for (int j = i + 1; j < a.length; ++j) {
-                    if (a[i].compareTo(a[j]) > 0) {
-                        String temp = a[i];
-                        a[i] = a[j];
-                        a[j] = temp;
+            for (int i = 0; i < field.length - 1; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (i > 0 && i < 8) {
+                        if (((field[i][j] == 1) && (field[i + 1][j] == 1)
+                                && (field[i + 2][j] == 0)) && (field[i - 1][j] == 0)) {
+                            destroyers++;
+                        }
+                    } else if (i == 0) {
+                        if (((field[i][j] == 1) && (field[i + 1][j] == 1)
+                                && (field[i + 2][j] == 0))) {
+                            destroyers++;
+                        }
+                    } else {
+                        if (((field[i][j] == 1) && (field[i + 1][j] == 1)
+                                && (field[i - 1][j] == 0))) {
+                            destroyers++;
+                        }
                     }
                 }
             }
-            Arrays.sort(a, Comparator.comparingInt(String::length).reversed());
-            StringBuilder str2 = new StringBuilder();
-            String separator = "";
 
-            for (String s : a) {
-                str2.append(separator);
-                str2.append(s);
-                separator = "/";
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if ((i > 0) && (j > 0) && (i < 9) && (j < 9)) {
+                        if ((field[i][j] == 1) && (field[i + 1][j] == 0) && (field[i][j + 1] == 0)
+                                && (field[i - 1][j]) == 0 && (field[i][j - 1] == 0)) {
+                            submarines++;
+                        }
+                    } else if ((i == 9) && (j < 9) && (j > 0)) {
+                        if ((field[i][j] == 1) && (field[i - 1][j] == 0) && (field[i][j + 1] == 0) && (field[i][j - 1] == 0)) {
+                            submarines++;
+                        }
+                    } else if ((j == 9) && (i < 9) && (i > 0)) {
+                        if ((field[i][j] == 1) && (field[i][j - 1] == 0) && (field[i + 1][j] == 0) && (field[i - 1][j] == 0)) {
+                            submarines++;
+                        }
+                    } else if ((i == 0) && (j < 9) && (j > 0)) {
+                        if ((field[i][j] == 1) && (field[i + 1][j] == 0) && (field[i][j + 1] == 0) && (field[i][j - 1] == 0)) {
+                            submarines++;
+                        }
+                    } else if ((j == 0) && (i < 9) && (i > 0)) {
+                        if ((field[i][j] == 1) && (field[i][j + 1] == 0) && (field[i + 1][j] == 0) && (field[i - 1][j] == 0)) {
+                            submarines++;
+                        }
+                    } else if ((i == 0) && (j == 9)) {
+                        if ((field[i][j] == 1) && (field[i + 1][j] == 0) && (field[i][j - 1] == 0)) {
+                            submarines++;
+                        }
+                    } else if ((i == 9) && (j == 0)) {
+                        if ((field[i][j] == 1) && (field[i - 1][j] == 0) && (field[i][j + 1] == 0)) {
+                            submarines++;
+                        }
+                    } else if ((i == 0) && (j == 0)) {
+                        if ((field[i][j] == 1) && (field[i + 1][j] == 0) && (field[i][j + 1] == 0)) {
+                            submarines++;
+                        }
+                    } else if ((i == 9) && (j == 9)) {
+                        if ((field[i][j] == 1) && (field[i - 1][j] == 0) && (field[i][j - 1] == 0)) {
+                            submarines++;
+                        }
+                    }
+                }
             }
-
-
-            return str2.toString();
+            System.out.println(battleship + " " + cruisers + " " + destroyers + " " + submarines);
+            return battleship == 1 && cruisers == 2 && destroyers == 3 && submarines == 4;
         }
     }
 }
